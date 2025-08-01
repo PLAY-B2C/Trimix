d32b4e189a4bdde25df8be3105108637ec94db36
PLAY-B2C <playgames3043@gmail.com>
2025-08-01 06:26:22 +0530

    Update cata.js

--- cata.js
+++ cata.js
@@ -6,7 +6,7 @@ function createBot() {
   const bot = mineflayer.createBot({
     host: 'mc.fakepixel.fun',
     port: 25565,
-    username: 'JamaaLcaliph',
+    username: 'DrakonTide',
     auth: 'offline',
     checkTimeoutInterval: 60000
   })
