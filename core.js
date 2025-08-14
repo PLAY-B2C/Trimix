d34e99d7f909911dddeb6c7fc71dcdb8d41fb5c1
PLAY-B2C <playgames3043@gmail.com>
2025-08-14 13:30:12 +0530

    Update and rename wolf.js to core.js

---
+++ core.js
@@ -0,0 +1,55 @@
+const mineflayer = require('mineflayer')
+
+const HOST = 'reserver.fakepixel.fun'
+const PORT = 25565
+
+// Bot usernames
+const BOT_NAMES = [
+  'DevXD',
+  'rogouemc'
+]
+
+// Delay before reconnect (in ms)
+const RECONNECT_DELAY = 5000
+
+function createBot(username) {
+  let bot
+  let reconnectTimeout = null
+
+  function start() {
+    bot = mineflayer.createBot({
+      host: HOST,
+      port: PORT,
+      username: username
+    })
+
+    bot.on('login', () => {
+      console.log(`[${username}] Logged in.`)
+    })
+
+    bot.on('end', (reason) => {
+      console.log(`[${username}] Disconnected: ${reason}`)
+      scheduleReconnect()
+    })
+
+    bot.on('error', (err) => {
+      console.log(`[${username}] Error: ${err.message}`)
+    })
+  }
+
+  function scheduleReconnect() {
+    if (reconnectTimeout) return // Prevent stacking reconnect timers
+    reconnectTimeout = setTimeout(() => {
+      reconnectTimeout = null
+      console.log(`[${username}] Reconnecting...`)
+      start()
+    }, RECONNECT_DELAY)
+  }
+
+  start()
+}
+
+// Start all bots
+BOT_NAMES.forEach(name => {
+  createBot(name)
+})
