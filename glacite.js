e28f6c68fc988c7780646ef65aba01d451039dc5
PLAY-B2C <playgames3043@gmail.com>
2025-08-20 18:20:37 +0530

    Update glacite.js

--- glacite.js
+++ glacite.js
@@ -185,8 +185,8 @@ bot.on('message', (jsonMsg) => {
   const msg = jsonMsg.toString().toLowerCase();
 
   if (
-    msg.includes('drakontide') ||
-    msg.includes('has sent you trade request') ||
+    msg.includes('he72818iwhshwj') ||
+    msg.includes('hdue7218iwjsj2i82') ||
     msg.includes('you were killed by')
   ) {
     console.log('📨 Trigger phrase detected. Disconnecting in 5s...');
